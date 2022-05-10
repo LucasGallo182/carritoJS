f81aafc44daebed8c81df8de613f3d8d129bfa90
Lucas Gallo <lucas.182g@gmail.com>
2022-05-09 21:01:35 -0300

    Agregando eventos

--- js/main.js
+++ js/main.js
@@ -1,150 +1,117 @@
-class Saludar {
-    constructor(usuario) {
-        this._usuario = usuario;
-    }
-    saludarlo() {
-        return `Hola ${this._usuario}, bienvenido a MallTop!`;
+//Variables
+const divCarrito = document.querySelector('#divItemCartContainer'); //querySelector se puede usar con class o #Id (el primero que encuentra de Id)
+const listaComponentes = document.querySelector('#contenedor-productos');
+const tableCarrito = document.querySelector('#listaCarrito tbody');
+const vaciarCarrito = document.querySelector('#vaciarCarrito');
+let carrito = [];
+
+cargarEventListeners();
+function cargarEventListeners() {
+    //Agregar componente cuando presiono boton
+    listaComponentes.addEventListener('click', agregarComponente);
+
+    //Eliminar componentes del carrito
+    divCarrito.addEventListener('click', eliminarComponente);
+
+    //Vaciar todos los componentes del carrito
+    vaciarCarrito.addEventListener('click', () =>{
+        carrito = []; //vacio el arreglo carrito
+        limpiarHTML(); //Se elimina todo el HTML con la funcion
+    })
+}
+
+//Funciones
+function agregarComponente(e) {
+    e.preventDefault();
+    if (e.target.classList.contains('agregarCarrito')){
+        const componenteSeleccionado = e.target.parentElement.parentElement.parentElement;
+        leerComponentes(componenteSeleccionado);
     }
 }
 
-//arrayCarritoVacio
-const carrito = [];
+//Eliminar componentes del carrito
+function eliminarComponente(e) {
+    if(e.target.classList.contains('eliminarComponente')){
+        const compId = e.target.getAttribute('data-id');
 
-const componentes = [{
-        nombre: 'Mouse ASUS Tuf',
-        precio: 3000
-    },
-    {
-        nombre: 'Mouse Corsair K20',
-        precio: 4000
-    },
-    {
-        nombre: 'Auriculares Logitech G435',
-        precio: 5000
-    },
-    {
-        nombre: 'Auriculares Logitech G733',
-        precio: 6000
-    },
-    {
-        nombre: 'Teclado Corsair K55',
-        precio: 5500
-    },
-    {
-        nombre: 'Teclado HyperX Alloy',
-        precio: 5200
-    },
-    {
-        nombre: 'Gabinete Deepcool Mesh 55',
-        precio: 8000
-    },
-    {
-        nombre: 'Gabinete Phanteks P300',
-        precio: 12000
-    }
-]
-
-class Computacion {
-    constructor(componente){
-        this.nombre = componente.nombre;
-        this.precio = componente.precio;
+        //Eliminar del carrito (array) por data-id
+        carrito = carrito.filter(componente => componente.id !== compId);
+        carritoHTML(); //Volvemos a mostrar el carrito HTML (actualizamos)
     }
 }
 
-function mostrarComponentes(){
-    let salida = "";
-    let i = 1;
+//Leer el contenido y ve que componente es
+function leerComponentes(componente) {
 
-    for (let componente of componentes){
-        salida += i + "." + componente.nombre + " - $" + componente.precio + "\n";
-        i++;
+    //Objeto con el contenido del componente
+    const infoComponente = {
+        imagen: componente.querySelector('img').src,
+        titulo: componente.querySelector('h5').textContent,
+        precio: Number(componente.querySelector('strong').textContent),
+        id: componente.querySelector('button').getAttribute('data-id'),
+        cantidad: 1
     }
-    return salida;
-}
 
-function buscarComponentes(){
-    seleccionIndice = parseInt(prompt("Elige el número de componente a buscar: \n1.Mouse \n2.Auriculares \n3.Teclados \n4.Gabinetes"));
-    let guardar = "Los componentes encontrados son: \n\n";
-    switch (seleccionIndice) {
-        case 1:
-            const buscaMouse = componentes.filter(componente => componente.nombre.includes('Mouse'));
-            for (let bMouse of buscaMouse){
-                guardar += bMouse.nombre + " - $" + bMouse.precio + "\n";
+    //Revisa si un elemento ya existe (para ver cantidad)
+    const yaExiste = carrito.some(componente => componente.id === infoComponente.id); //el some verifica si en un arreglo ya existe el objeto
+    if(yaExiste) {
+        //Actualizamos cantidad
+        const componentes = carrito.map(componente =>{ //Me crea un nuevo arreglo el map
+            if(componente.id === infoComponente.id){ //cuando el componente ya agregado al carrito sea igual que el que intentamos agregar
+                componente.cantidad++;
+                return componente; //retorna objeto actualizado
+            } else {
+                return componente; //retorna objetos que no son duplicados
             }
-            alert(guardar);
-            break;
-        case 2:
-            const buscaAuricualares = componentes.filter(componente => componente.nombre.includes('Auriculares'));
-            for (let bAuris of buscaAuricualares){
-                guardar += bAuris.nombre + " - $" + bAuris.precio + "\n";
-            }
-            alert(guardar);
-            break;
-        case 3:
-            const buscaTeclado = componentes.filter(componente => componente.nombre.includes('Teclado'));
-            for (let bTeclado of buscaTeclado){
-                guardar += bTeclado.nombre + " - $" + bTeclado.precio + "\n";
-            }
-            alert(guardar);
-            break;
-        case 4:
-            const buscaGabinete = componentes.filter(componente => componente.nombre.includes('Gabinete'));
-            for (let bGabinete of buscaGabinete){
-                guardar += bGabinete.nombre + " - $" + bGabinete.precio + "\n";
-            }
-            alert(guardar);
-            break;
-        default:
-            alert("La opción ingresada no es correcta, intente nuevamente");
-    }
-    opciones = prompt("Por favor ingrese el número de opción: \n1.Comprar \n2.Buscar producto \n3.Salir");
-}
-
-function agregarAlCarrito(componente){
-    carrito.push(componente);
-}
-
-function mostrarCarrito(){
-    let muestra = "Componentes comprados:\n";
-    let total = 0;
-    
-    for (let componente of carrito){
-        muestra += "Componente: " + componente.nombre + " - Precio: $" + componente.precio + "\n";
-        total += parseInt(componente.precio);
-    }
-
-    muestra += "\nPrecio total: $" + total;
-    alert(muestra);
-    let divMostrar = document.getElementById("mostrar");
-    divMostrar.innerHTML = muestra;
-}
-
-function comprarComponente(){
-    let salir = "";
-
-    while(salir != 2) {
-            let listaComponentes = mostrarComponentes();
-            let componenteElegido = parseInt(prompt("Seleccione el número de componente que desea comprar: \n\n" + listaComponentes));
-            let comps = componentes[(componenteElegido - 1)]; //porque arranca de cero
-            agregarAlCarrito(comps);
-            salir = parseInt((prompt("Eliga el número de opción: \n1.Agregar otro componente \n2.Finalizar y ver carrito")));
-    }
-}
-
-let usuarioNuevo = new Saludar(prompt('Ingresá tu nombre de usuario:'));
-alert(usuarioNuevo.saludarlo());
-
-let opciones = prompt("Por favor ingrese el número de opción: \n1.Comprar \n2.Buscar producto \n3.Salir");
-
-while (opciones !== 3){
-    if (opciones == 1){
-        comprarComponente();
-    }
-    if (opciones == 2){
-        buscarComponentes();
+        })
+        carrito = [...componentes]; //...representan al spread operator transforma la variable carrito en una lista de argumentos (para ver la lista completa del array de map)
     } else {
-        mostrarCarrito();
-        opciones = 3;
-        alert("Gracias por su compra");
+        //Agregamos el componente nuevo
+        carrito = [...carrito, infoComponente];
+    }
+
+    //LLamo a funcion para que muestre el HTML del carrito
+    carritoHTML();
+}
+
+//Mostrar carrito en HTML
+function carritoHTML() {
+
+    //Limpiar HTML
+    limpiarHTML();
+
+    //Recorre el array y general HTML
+    carrito.forEach(componente =>{
+        let subtotal = componente.precio * componente.cantidad;
+        let total = 0;
+        total += subtotal;
+        const tr = document.createElement('tr');
+        tr.innerHTML = `
+        <td>
+            <img src="${componente.imagen}" class="iconCartSize">
+        </td>
+        <td>
+            ${componente.titulo}
+        </td>
+        <td>
+            <strong>$${componente.precio}</strong>
+            Cant.: ${componente.cantidad}
+            Sub-Total: $${subtotal}
+        </td>
+        <td>
+            <button type="button" class="btn btn-danger btn-sm btnCart"><img src="./img/trash-icon.png" class="iconTrashSize eliminarComponente" data-id="${componente.id}"></button>
+        </td>
+        `
+        //Agregar el HTML en el tbody
+        tableCarrito.appendChild(tr);
+    })
+}
+
+//Elimina los componentes del tbody que se repiten
+function limpiarHTML() {
+    //Si tiene al menos un elemento se sigue ejecutando, si se limpia el HTML ya no se ejecuta (eliminar elementos hijos)
+    while(tableCarrito.firstChild){
+        tableCarrito.removeChild(tableCarrito.firstChild)
     }
 }
+
